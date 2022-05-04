8a6a770c3078de22c4bcff2ed8efbc1c8761851e
Issa Memari <issa.memari@gmail.com>
2022-05-04 22:50:05 +0200

    Improve jump-game-ii

--- medium/jump-game-ii/solution.py
+++ medium/jump-game-ii/solution.py
@@ -3,30 +3,28 @@ from typing import List
 
 class Solution:
 
-    def __init__(self):
-        self.cache = {}
+    def jump(self, nums: List[int]) -> int:
 
-    def jumpRecursive(self, nums: List[int], index: int):
-
-        if len(nums) <= 1:
+        if len(nums) == 1:
             return 0
 
-        if index in self.cache:
-            return self.cache[index]
+        lastJump = 1
+        lastIndex = 0
 
-        minJumps = float("inf")
-        num = nums[index]
+        for i in range(3, len(nums) + 1):
+            lastJumpNum = nums[lastIndex]
+            if lastIndex + lastJumpNum < i - 1:
+                maxDistInd = None
+                maxDist = -1
+                j = i - 2
+                while j > lastIndex:
+                    if nums[j] + j + 1 - i > maxDist:
+                        maxDist = nums[j] + j + 1 - i
+                        maxDistInd = j
 
-        if num + index >= len(nums) - 1:
-            return 1
+                    j -= 1
 
-        for i in range(1, num + 1):
-            jumps = 1 + self.jumpRecursive(nums, index + i)
-            if jumps < minJumps:
-                minJumps = jumps
+                lastJump += 1
+                lastIndex = maxDistInd
 
-        self.cache[index] = minJumps
-        return minJumps
-
-    def jump(self, nums: List[int]) -> int:
-        return self.jumpRecursive(nums, 0)
+        return lastJump
