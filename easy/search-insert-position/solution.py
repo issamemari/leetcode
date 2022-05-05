c8b647531a9ee0bc3484d6d5d508dd0d732ac181
Issa Memari <issa.memari@gmail.com>
2022-05-06 00:02:33 +0200

    Solve search-insert-position

---
+++ easy/search-insert-position/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+
+    def binarySearchInsert(self, nums: List[int], target: int, begin: int, end: int):
+        if begin >= end:
+            if target <= nums[begin]:
+                return begin
+            return begin + 1
+
+        mid = (begin + end) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] > target:
+            return self.binarySearchInsert(nums, target, begin, mid - 1)
+
+        return self.binarySearchInsert(nums, target, mid + 1, end)
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return self.binarySearchInsert(nums, target, 0, len(nums) - 1)
