2e01cb2c18f9dbdb7b95692562469c3148106ff8
Issa Memari <issa.memari@gmail.com>
2022-05-12 19:20:16 +0200

    Solve permutations-ii

---
+++ medium/permutations-ii/solution.py
@@ -0,0 +1,50 @@
+class Solution:
+
+    def binarySearch(self, nums, target, begin, end):
+        """
+        This binary search function finds the last element
+        in the list that is larger than the give target.
+
+        Assumes the list is sorted in descending order.
+        """
+        while begin <= end:
+            mid = (begin + end) // 2
+            if nums[mid] > target:
+                if mid + 1 <= end and nums[mid + 1] > target:
+                    begin = mid + 1
+                    continue
+
+                return mid
+
+            end = mid - 1
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 1
+        while i > 0 and nums[i - 1] >= nums[i]:
+            i -= 1
+
+        if i == 0:
+            nums.reverse()
+            return
+
+        index = self.binarySearch(nums, nums[i - 1], i, len(nums) - 1)
+        nums[i - 1], nums[index] = nums[index], nums[i - 1]
+
+        for j in range((len(nums) - i) // 2):
+            nums[i + j], nums[len(nums) - j - 1] = nums[len(nums) - j - 1], nums[i + j]
+
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        first = tuple(nums)
+
+        res = [list(nums)]
+
+        self.nextPermutation(nums)
+        while tuple(nums) != first:
+            res.append(list(nums))
+            self.nextPermutation(nums)
+
+        return res
