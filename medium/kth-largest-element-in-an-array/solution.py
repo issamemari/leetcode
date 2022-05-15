7a368d1f54b1a35b9b6261f9b52cafe5dbdce2d2
Issa Memari <issa.memari@gmail.com>
2022-05-15 11:27:58 +0200

    Solve kth-largest-element-in-an-array

---
+++ medium/kth-largest-element-in-an-array/solution.py
@@ -0,0 +1,15 @@
+import heapq
+
+from typing import List
+
+
+class Solution:
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        heapq._heapify_max(nums)
+
+        for i in range(k):
+            res = heapq._heappop_max(nums)
+
+        return res
