334fc15671b9f61a1f635adde348cee4de686545
Issa Memari <issa.memari@gmail.com>
2022-05-16 22:56:58 +0200

    Solve shortest-path-in-binary-matrix

---
+++ medium/shortest-path-in-binary-matrix/solution.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+
+        if grid[0][0] == 1:
+            return -1
+
+        queue, visited = [((0, 0), 1)], set()
+
+        neighbors = [
+            (1, 1),
+            (0, 1),
+            (1, 0),
+            (1, -1),
+            (-1, 1),
+            (-1, 0),
+            (0, -1),
+            (-1, -1),
+        ]
+
+        n = len(grid)
+
+        while queue:
+
+            (i, j), length = queue.pop(0)
+
+            if (i, j) in visited:
+                continue
+
+            visited.add((i, j))
+
+            if (i, j) == (n - 1, n - 1):
+                return length
+
+            for k, l in neighbors:
+                if 0 <= i + k < n and 0 <= j + l < n and grid[i + k][j + l] == 0:
+                    queue.append(((i + k, j + l), length + 1))
+
+        return -1
