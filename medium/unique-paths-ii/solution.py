7c73cb1df403615842763192ece3355367bea5ee
Issa Memari <issa.memari@gmail.com>
2023-10-28 14:47:20 +0200

    Add another solution for unique-paths-ii

--- medium/unique-paths-ii/solution.py
+++ medium/unique-paths-ii/solution.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
 
     def __init__(self):
         self.memory = {}
@@ -32,3 +32,42 @@ class Solution:
 
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         return self.helper(0, 0, obstacleGrid)
+
+
+class Solution2:
+
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        countGrid = [
+            [0 for _ in range(n)]
+            for _ in range(m)
+        ]
+
+        queue = [(m - 1, n - 1)]
+        visited = set()
+        while len(queue) > 0:
+            row, col = queue.pop(0)
+
+            if (row, col) in visited:
+                continue
+            
+            visited.add((row, col))
+
+            if obstacleGrid[row][col] != 1:
+                if row == m - 1 and col == n - 1:
+                    countGrid[row][col] = 1
+
+                if row < m - 1:
+                    countGrid[row][col] += countGrid[row + 1][col]
+
+                if col < n - 1:
+                    countGrid[row][col] += countGrid[row][col + 1]
+
+            if row > 0:
+                queue.append((row - 1, col))
+
+            if col > 0:
+                queue.append((row, col - 1))
+
+        return countGrid[0][0]
